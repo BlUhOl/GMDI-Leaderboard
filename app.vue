3b97dee5ee6834c55ddac49bdc222c35bef5d956
BlUhOl <92349922+BlUhOl@users.noreply.github.com>
2024-01-26 17:48:28 +0700

    trying to add seo

--- app.vue
+++ app.vue
@@ -7,4 +7,27 @@
   </NuxtLayout>
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: 'GMDI Leaderboard',
+  ogTitle: 'GMDI Leaderboard',
+  description: 'Indonesian Geometry Dash player Leaderboard',
+  ogDescription: 'Indonesian Geometry Dash player Leaderboard',
+  ogImage: '/assets/img/page_thumb.png',
+})
+</script>
 
+<!-- <script>
+export default {
+  head() {
+    return {
+      title: 'GMDI Leaderboard',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Indonesian Geometry Dash player Leaderboard' },
+        { hid: 'og:image', property: 'og:image', content: '/assets/img/page_thumb/png' },
+        { name: 'theme-color', content: '#3498db' }, // Replace with your desired color
+      ],
+    };
+  },
+};
+</script> -->
