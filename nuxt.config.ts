ab0b0efc17e2d441566292fd087873da583eca5f
BlUhOl <92349922+BlUhOl@users.noreply.github.com>
2025-12-15 12:01:04 +0700

    Remove route rules for path redirection
    Removed route rules for redirecting paths.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {    
-    '/:path/**': {      
-      redirect: {        
-        to: 'https://gmdilb.pages.dev/',        
-        statusCode: 301       
-      }    
-    }  
-  },
+ 
   devtools: { enabled: true },
   hub: {
     cache: true
